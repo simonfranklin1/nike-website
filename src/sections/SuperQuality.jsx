27bfd80f22977ebfc3079af46de953cf746d0d9c
Simon Franklin <simonfranklin.54@gmail.com>
2024-06-12 04:58:47 -0300

    refactor: Testando novo componente Button

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shoe8 } from '../assets/images'
-import { Button } from '../../testComponents'
+import { Button, Text } from '../../testComponents'
 
 const SuperQuality = () => {
   return (
@@ -13,12 +13,12 @@ const SuperQuality = () => {
           <span className="text-coral-red">Quality</span> Shoes
         </h2>
         <p className="mt-4 lg:max-w-lg info-text">
-          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+          <Text text={"Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance."} />
         </p>
         <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
         <div className="mt-11 flex flex-wrap gap-4">
-          <Button children={"View details"} background={"rgb(255 100 82)"} borderRadius={"9999px"} textColor={"#fff"} padding={"1rem 1.75rem"} />
-          <Button children={"Learn more"} background="#fff" border={"1px solid rgb(109 109 109)"} borderRadius={"9999px"} textColor="rgb(109 109 109)" padding={"1rem 1.75rem"} />
+          <Button onClick={() => console.log("View details")} children={"View details"} background={"rgb(255 100 82)"} borderRadius={"9999px"} textColor={"#fff"} padding={"1rem 1.75rem"} />
+          <Button onClick={() => console.log("Learn more")} children={"Learn more"} background="#fff" border={"1px solid rgb(109 109 109)"} borderRadius={"9999px"} textColor="rgb(109 109 109)" padding={"1rem 1.75rem"} />
         </div>
       </div>
       <div className="flex-1 flex justify-center items-center">
